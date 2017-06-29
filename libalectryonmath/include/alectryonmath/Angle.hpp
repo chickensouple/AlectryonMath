7b8feac9d41e93a058df6b8072decacec067aa75
Clark <clarkz@seas.upenn.edu>
2017-06-29 00:46:52 -0400

    angle interpolation

--- libalectryonmath/include/alectryonmath/Angle.hpp
+++ libalectryonmath/include/alectryonmath/Angle.hpp
@@ -23,15 +23,51 @@ constexpr T TwoPi();
 template<class T>
 constexpr T HalfPi();
 
+/**
+ * @brief Constrains an angle to the interval [-pi, pi)
+ * @tparam T
+ * @param angle angle to constrain
+ * @return constrained angle
+ */
 template<class T>
 T constrain_angle_pi(T angle);
 
+/**
+ * @brief Constrains an angle to the interval [0, 2 pi)
+ * @tparam T
+ * @param angle angle to constrain
+ * @return constrained angle
+ */
 template<class T>
 T constrain_angle_two_pi(T angle);
 
+/**
+ * @brief Calculates the shortest angular distance between two angles
+ * @details Angles are treated by their equivalence class (eg. angles in the range [0, 2 pi))
+ * So the distance between angle1 and angle2 is the same as the distance between angle1 and angle2 + 2 pi
+ * The distance is symmetric. The distance between -0.1 and 0.1 is 0.2. The distance between 2 pi - 0.2 and 2 pi is 0.2
+ *
+ *
+ * @tparam T
+ * @param angle1
+ * @param angle2
+ * @return the shortest angular distance (in a range [0, pi))
+ */
 template<class T>
 T angle_dist(T angle1, T angle2);
 
+/**
+ * @brief Interpolates between two angles
+ * @details angles are treated as if equal to their equivalence class (eg. angles in the range [0, 2 pi))
+ * We obtain the smallest (in terms of magnitude) signed distance between angle1 and angle2
+ * The interpolation then returns angle1 + (dist * alpha).
+ *
+ * @tparam T
+ * @param angle1
+ * @param angle2
+ * @param alpha
+ * @return interpolated angle
+ */
 template<class T>
 T angle_interp(T angle1, T angle2, T alpha);
 
@@ -85,7 +121,16 @@ T angle_dist(T angle1, T angle2) {
 
 template<class T>
 T angle_interp(T angle1, T angle2, T alpha) {
+    T dist = constrain_angle_two_pi<T>(angle2 - angle1);
+    T interp;
+    if (dist > Pi<T>()) {
+        dist = TwoPi<T>() - dist;
+        interp = angle1 - alpha * dist;
+    } else {
+        interp = angle1 + alpha * dist;
+    }
 
+    return interp;
 }
 
 }
--- tests/AngleUnitTests.cpp
+++ tests/AngleUnitTests.cpp
@@ -12,130 +12,176 @@ using namespace Alectryon;
 
 template<class T>
 void TestConstants() {
-	CHECK_FLOATING_POINT(T, Angle::Pi<T>(), (T) M_PI);
-	CHECK_FLOATING_POINT(T, Angle::TwoPi<T>(), (T) (2 * M_PI));
-	CHECK_FLOATING_POINT(T, Angle::HalfPi<T>(), (T) (0.5 * M_PI));
+    CHECK_FLOATING_POINT(T, Angle::Pi<T>(), (T) M_PI);
+    CHECK_FLOATING_POINT(T, Angle::TwoPi<T>(), (T) (2 * M_PI));
+    CHECK_FLOATING_POINT(T, Angle::HalfPi<T>(), (T) (0.5 * M_PI));
 }
 
 template<class T>
 void TestConstrainAngle() {
-	T angle1 = 0.0;
-	T angle2 = Angle::TwoPi<T>() + (T) 0.2;
-	T angle3 = Angle::Pi<T>() + (T) 0.3;
-	T angle4 = Angle::TwoPi<T>() * 6 + (T) (-0.2);
-	T angle5 = -Angle::TwoPi<T>() + (T) 0.1;
-	T angle6 = -Angle::Pi<T>() + (T) (-0.21);
-	T angle7 = -Angle::TwoPi<T>() * 4 + (T) 0.1;
+    T angle1 = 0.0;
+    T angle2 = Angle::TwoPi<T>() + (T) 0.2;
+    T angle3 = Angle::Pi<T>() + (T) 0.3;
+    T angle4 = Angle::TwoPi<T>() * 6 + (T) (-0.2);
+    T angle5 = -Angle::TwoPi<T>() + (T) 0.1;
+    T angle6 = -Angle::Pi<T>() + (T) (-0.21);
+    T angle7 = -Angle::TwoPi<T>() * 4 + (T) 0.1;
 
-	// test when angle doesn't need to be constrained
-	CHECK_FLOATING_POINT(T, angle1, Angle::constrain_angle_pi<T>(angle1));
-	CHECK_FLOATING_POINT(T, angle1, Angle::constrain_angle_two_pi<T>(angle1));
+    // test when angle doesn't need to be constrained
+    CHECK_FLOATING_POINT(T, angle1, Angle::constrain_angle_pi<T>(angle1));
+    CHECK_FLOATING_POINT(T, angle1, Angle::constrain_angle_two_pi<T>(angle1));
 
-	// test when angle is over 2 pi
-	CHECK_FLOATING_POINT(T, (T) 0.2, Angle::constrain_angle_two_pi(angle2));
-	// test when angle is over pi
-	CHECK_FLOATING_POINT(T, (T) 0.3 - Angle::Pi<T>(), Angle::constrain_angle_pi(angle3));
+    // test when angle is over 2 pi
+    CHECK_FLOATING_POINT(T, (T) 0.2, Angle::constrain_angle_two_pi(angle2));
+    // test when angle is over pi
+    CHECK_FLOATING_POINT(T, (T) 0.3 - Angle::Pi<T>(), Angle::constrain_angle_pi(angle3));
 
-	// test when angle is way over 2 pi
-	CHECK_FLOATING_POINT(T, (T) (-0.2) + Angle::TwoPi<T>(), Angle::constrain_angle_two_pi(angle4));
+    // test when angle is way over 2 pi
+    CHECK_FLOATING_POINT(T, (T) (-0.2) + Angle::TwoPi<T>(), Angle::constrain_angle_two_pi(angle4));
 
-	// test when angle is below two pi
-	CHECK_FLOATING_POINT(T, (T) 0.1, Angle::constrain_angle_two_pi(angle5));
-	// test when angle is below pi
-	CHECK_FLOATING_POINT(T, (T) (-0.21) + Angle::Pi<T>(), Angle::constrain_angle_pi(angle6));
+    // test when angle is below two pi
+    CHECK_FLOATING_POINT(T, (T) 0.1, Angle::constrain_angle_two_pi(angle5));
+    // test when angle is below pi
+    CHECK_FLOATING_POINT(T, (T) (-0.21) + Angle::Pi<T>(), Angle::constrain_angle_pi(angle6));
 
-	// test when angle is way under two pi
-	CHECK_FLOATING_POINT(T, (T) 0.1, Angle::constrain_angle_pi(angle7));
+    // test when angle is way under two pi
+    CHECK_FLOATING_POINT(T, (T) 0.1, Angle::constrain_angle_pi(angle7));
 }
 
 template<class T>
 void TestAngleDist() {
-	// testing simplest case
-	T angle1 = 0.1;
-	T angle2 = 0.2;
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0.1);
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0.1);
+    // testing simplest case
+    T angle1 = 0.1;
+    T angle2 = 0.2;
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0.1);
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0.1);
 
-	// test when angles are negative
-	angle1 = -0.1;
-	angle2 = -0.2;
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0.1);
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0.1);
+    // test when angles are negative
+    angle1 = -0.1;
+    angle2 = -0.2;
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0.1);
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0.1);
 
-	// test when one angle is positive, one is negative
-	angle1 = -0.1;
-	angle2 = 0.2;
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0.3);
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0.3);
+    // test when one angle is positive, one is negative
+    angle1 = -0.1;
+    angle2 = 0.2;
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0.3);
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0.3);
 
-	// test when angles are the same
-	angle1 = 1.4;
-	angle2 = 1.4;
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0);
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0);
-	angle1 = -2.1;
-	angle2 = -2.1;
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0);
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0);
+    // test when angles are the same
+    angle1 = 1.4;
+    angle2 = 1.4;
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0);
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0);
+    angle1 = -2.1;
+    angle2 = -2.1;
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0);
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0);
 
-	// test when angles are around two pi
-	angle1 = Angle::TwoPi<T>() - 0.05;
-	angle2 = Angle::TwoPi<T>() + 0.05;
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0.1);
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0.1);
-	angle1 = Angle::TwoPi<T>() - 0.05;
-	angle2 = 0.05;
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0.1);
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0.1);
+    // test when angles are around two pi
+    angle1 = Angle::TwoPi<T>() - 0.05;
+    angle2 = Angle::TwoPi<T>() + 0.05;
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0.1);
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0.1);
+    angle1 = Angle::TwoPi<T>() - 0.05;
+    angle2 = 0.05;
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0.1);
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0.1);
 
-	// test when angles are way above or below two pi
-	angle1 = 4 * Angle::TwoPi<T>() + 0.5;
-	angle2 = 6 * Angle::TwoPi<T>() + 0.82;
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0.32);
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0.32);
-	angle1 = 4 * Angle::TwoPi<T>() + 0.5;
-	angle2 = -6 * Angle::TwoPi<T>() + 0.82;
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0.32);
-	CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0.32);
+    // test when angles are way above or below two pi
+    angle1 = 4 * Angle::TwoPi<T>() + 0.5;
+    angle2 = 6 * Angle::TwoPi<T>() + 0.82;
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0.32);
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0.32);
+    angle1 = 4 * Angle::TwoPi<T>() + 0.5;
+    angle2 = -6 * Angle::TwoPi<T>() + 0.82;
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle1, angle2), (T) 0.32);
+    CHECK_FLOATING_POINT(T, Angle::angle_dist<T>(angle2, angle1), (T) 0.32);
 }
 
-template <class T>
+template<class T>
 void TestAngleInterp() {
     T angle1 = 0;
-    T angle2 = Angle::Pi<T>();
+    T angle2 = Angle::HalfPi<T>();
 
-    //CHECK_FLOATING_POINT(T, Angle::angle_interp(angle1, angle2, (T) 0.5), Angle::HalfPi<T>());
+    // basic test checking that halfway points are equal
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle1, angle2, (T) 0.5), Angle::HalfPi<T>() * 0.5);
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle2, angle1, (T) 0.5), Angle::HalfPi<T>() * 0.5);
+
+    // basic tests for other alphas
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle1, angle2, (T) 0.2), Angle::HalfPi<T>() * 0.2);
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle2, angle1, (T) 0.2), Angle::HalfPi<T>() * 0.8);
+
+    // tests when angles straddle the 0/2pi crossing
+    angle1 = -Angle::HalfPi<T>() / 2;
+    angle2 = Angle::HalfPi<T>() / 2;
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle1, angle2, (T) 0.5), 0);
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle2, angle1, (T) 0.5), 0);
+
+
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle1, angle2, (T) 0.1), -0.8 * Angle::HalfPi<T>() / 2);
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle2, angle1, (T) 0.1), 0.8 * Angle::HalfPi<T>() / 2);
+
+    // test when angles straddle pi
+    angle1 = Angle::Pi<T>() - (Angle::HalfPi<T>() / 2);
+    angle2 = Angle::Pi<T>() + (Angle::HalfPi<T>() / 2);
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle1, angle2, (T) 0.5), Angle::Pi<T>());
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle2, angle1, (T) 0.5), Angle::Pi<T>());
+
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle1, angle2, (T) 0.3), Angle::Pi<T>() - 0.4 * (Angle::HalfPi<T>() / 2));
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle2, angle1, (T) 0.3), Angle::Pi<T>() + 0.4 * (Angle::HalfPi<T>() / 2));
+
+    // test corner cases
+    angle1 = 0.4;
+    angle2 = 0.8;
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle1, angle2, (T) 0), angle1);
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle1, angle2, (T) 1), angle2);
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle2, angle1, (T) 0), angle2);
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle2, angle1, (T) 1), angle1);
+
+    // testing random angles
+    angle1 = 0.2;
+    angle2 = 0.8;
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle1, angle2, (T) 0.1), 0.26);
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle2, angle1, (T) 0.1), 0.74);
+
+    // testing nonstandard inputs, alpha not in range [0, 1]
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle1, angle2, (T) 2.5), 1.7);
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle2, angle1, (T) 2.5), -0.7);
+
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle1, angle2, (T) -1.5), -0.7);
+    CHECK_FLOATING_POINT(T, Angle::angle_interp(angle2, angle1, (T) -1.5), 1.7);
 }
 
 TEST(Angle, Constants) {
-	// testing float version
-	TestConstants<float>();
+    // testing float version
+    TestConstants<float>();
 
-	// testing double version
-	TestConstants<double>();
+    // testing double version
+    TestConstants<double>();
 
-	// testing long double version
-	TestConstants<long double>();
+    // testing long double version
+    TestConstants<long double>();
 }
 
 
 TEST(Angle, ConstrainAngle) {
-	// testing float version
-	TestConstrainAngle<float>();
+    // testing float version
+    TestConstrainAngle<float>();
 
-	// testing double version
-	TestConstrainAngle<double>();
+    // testing double version
+    TestConstrainAngle<double>();
 }
 
 TEST(Angle, AngleDist) {
-	// testing float version
-	TestAngleDist<float>();
+    // testing float version
+    TestAngleDist<float>();
 
-	// testing double version
-	TestAngleDist<double>();
+    // testing double version
+    TestAngleDist<double>();
 }
 
-TEST(angle, AngleInterp) {
+TEST(Angle, AngleInterp) {
     // testing float version
     TestAngleInterp<float>();
 
@@ -144,7 +190,7 @@ TEST(angle, AngleInterp) {
 }
 
 int main(int argc, char *argv[]) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
 
